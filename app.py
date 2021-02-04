5934682dd54c055db5586f2d379e1568df90fa03
Alexandru Apostoiu <alexander.apostoiu@gmail.com>
2021-02-04 10:59:48 +0000

    wired up edit button with the template

--- app.py
+++ app.py
@@ -125,6 +125,14 @@ def add_recipe():
     return render_template("add_recipe.html", categories=all_categories)
 
 
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template(
+        "edit_recipe.html", recipe=recipe, categories=categories)
+
+
 @app.route('/view_categories/<category_name>')
 def view_categories(category_name):
     all_recipes = mongo.db.recipes.find({"category_name": category_name})
